93b92f52b3624d47a5b4df770808e588aee17e7d
Amy Lee <amyleeannebennett@gmail.com>
2016-03-03 10:44:16 -0600

    socket is connected

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 'use strict';
 
-const express = require('express')
-const app = express()
-const server = require('http').createServer(app) //express() raw node
-const io = require('socket.io')(server)
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app); //express() raw node
+const ws = require('socket.io')(server);
 
 const PORT = process.env.PORT || 3000;
 
+app.set('view engine', 'jade');
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('hello')
-})
+  res.render('index');
+});
 
 server.listen(PORT, () => {
   console.log(`Server is listening on PORT ${PORT}`);
+});
+
+ws.on('connection', socket => {
+  console.log("connection", socket);
 });
